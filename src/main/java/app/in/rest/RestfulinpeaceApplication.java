ee0ce16f9e9d9e4106bb0a510f949cd228cf8ad7
javagrails <salman1m22@gmail.com>
2017-07-19 15:29:58 +0600

    In Sha Allah project started

---
+++ src/main/java/app/in/rest/RestfulinpeaceApplication.java
@@ -0,0 +1,12 @@
+package app.in.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulinpeaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulinpeaceApplication.class, args);
+	}
+}
