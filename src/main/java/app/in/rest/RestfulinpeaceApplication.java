aeb483438efe51334166015df9e2391b632e8060
javagrails <salman1m22@gmail.com>
2017-07-21 00:21:50 +0600

    ServletInitializer.java+RestfulinpeaceApplication.java  => updated

--- src/main/java/app/in/rest/RestfulinpeaceApplication.java
+++ src/main/java/app/in/rest/RestfulinpeaceApplication.java
@@ -3,10 +3,19 @@ package app.in.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author M.M. SALEH (Salman)
+ * @designation Senior Software Engineer
+ * @github https://github.com/javagrails
+ * @fileName ServletInitializer.java | app.in.rest | ServletInitializer
+ * @since Jul 19, 2017 14:34:56 PM
+ */
+
+
 @SpringBootApplication
 public class RestfulinpeaceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestfulinpeaceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulinpeaceApplication.class, args);
+    }
 }
--- src/main/java/app/in/rest/ServletInitializer.java
+++ src/main/java/app/in/rest/ServletInitializer.java
@@ -3,6 +3,15 @@ package app.in.rest;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+
+/**
+ * @author M.M. SALEH (Salman)
+ * @designation Senior Software Engineer
+ * @github https://github.com/javagrails
+ * @fileName ServletInitializer.java | app.in.rest | ServletInitializer
+ * @since Jul 19, 2017 14:34:56 PM
+ */
+
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
